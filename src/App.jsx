f79c9a0083abb76f8b5e70cc3914dd4d57ea651d
Ridwan Taufiqulhakim <ridwanforever13@gmail.com>
2024-10-21 14:24:49 +0700

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 
-import Navbar from './navbar'
-import Hero from './hero'
-import Features from './Features'
+import Navbar from './landingPage/navbar'
+import Hero from './landingPage/hero'
+import Features from './landingPage/features'
 
 
 function App() {

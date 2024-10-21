50adb6db128be161f3dac77cd58906e8f7100bc2
Ridwan Taufiqulhakim <ridwanforever13@gmail.com>
2024-10-21 15:02:33 +0700

    hero changes

--- src/landingPage/features.jsx
+++ src/landingPage/features.jsx
@@ -4,7 +4,10 @@ import './Features.css';
 const Features = () => {
   return (
     <section className="features">
-      <h2>Keunggulan Sahabat Jasa</h2>
+        <div className='h2'>
+        <h2>Keunggulan Sahabat Jasa</h2>
+        </div>
+
       <div className="feature-item">
         <h3>Pelayanan Sempurna</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
